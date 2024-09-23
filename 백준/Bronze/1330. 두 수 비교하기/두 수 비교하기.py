a9c517bf31522e9012fc837b218e6d5aa79be6f6
Gyu <qkrckdrb4106@gmail.com>
2024-09-23 09:36:32 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,10 @@
+A, B = input().split()
+A = int(A)
+B = int(B)
+if -10000 <= A <= 10000 and -10000 <= B <= 10000:
+    if A > B:
+        print(">")
+    if A < B:
+        print("<")
+    if A == B:
+        print("==")
