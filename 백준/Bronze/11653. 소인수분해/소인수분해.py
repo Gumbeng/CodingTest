098063183e373686021d72a1fd254d9c3122ca9a
Gyu <qkrckdrb4106@gmail.com>
2024-09-26 19:52:42 +0900

    [Bronze I] Title: 소인수분해, Time: 852 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,7 +1,8 @@
 N = int(input())
 for i in range(2, N+1):
-    while N % i == 0: # 같은 소인수로 나눌 수 있을 때가지 반복
-        print(i)
-        N //= i
-    if N == 1: # N이 1이되면 더이상 나눌 필요가 없음
-        break
+    if N % i == 0:
+        while N % i == 0: # 같은 소인수로 나눌 수 있을 때가지 반복
+            print(i)
+            N //= i
+        if N == 1: # N이 1이되면 더이상 나눌 필요가 없음
+            break
