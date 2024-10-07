d018e246ac3c090b518dc29097d608b29da56318
Gyu <qkrckdrb4106@gmail.com>
2024-10-07 09:37:59 +0900

    [Bronze I] Title: 최소공배수, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,11 @@
+N = int(input())
+for _ in range(N):
+    n, m = map(int, input().split())
+    x = n * m
+    while m != 0:
+        if n < m:
+            n,m = m,n
+        t = n % m
+        n = m
+        m = t
+    print(int(x / n))
